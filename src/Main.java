08d6131dfd4ed0598677d2aff124b1f71ae48c20
GidJoe <winterm@bfwob.de>
2023-01-17 11:32:05 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,40 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        double[][] kinosaal = new double[10][13];
-        kinosaal[4] = new double[9];
-        kinosaal[5] = new double[9];
+        double[][] kinosaal = new double[10][16];
+        kinosaal[4] = new double[10];
+        kinosaal[5] = new double[10];
 
         double vortag = Math.random() * 50;
         for (int monat = 0; monat < kinosaal.length; monat++) {
             for (int tag = 0; tag < kinosaal[monat].length; tag++) {
                 kinosaal[monat][tag] = vortag;
                 vortag = vortag + Math.random() * 20 - 10;
+                if (vortag > 1) vortag = 1;
+                if (vortag < 1) vortag = 0;
             }
         }
 
-        System.out.print(Arrays.deepToString(kinosaal));
+        int dreier = 0;
+        int dreierfrei = 0;
+        String ausgabe = "";
+
+        for (int r = 0; i < kinosaal.length; i++) {
+            for (int p = 0; p < kinosaal[p].length; p++)
+                if (kinosaal[i][p] == 1) {
+                    dreier++;
+                    ausgabe+="[x]";
+                } else {ausgabe+="[__]";}
+
+            if (dreier == 3) {
+                dreierfrei++;
+                dreier = 0;
+            }
+
+        }
+
+        System.out.print(Arrays.toString(kinosaal[2]));
+        System.out.print(ausgabe);
+        System.out.print(dreierfrei);
     }
 }
